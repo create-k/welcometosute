1141b4df748ba435ce8fd62841abb9b2830fc765
create-k <131327436+create-k@users.noreply.github.com>
2025-01-07 12:43:53 +0900

    Add files via upload

--- timeline/script.js
+++ timeline/script.js
@@ -1,23 +1,9 @@
-// JavaScript
-//console.log('Hello world!');
-
-// 定数の定義
-const loading = document.getElementById('loading');
-
-// cssクラスを追加する関数
-function animation(){
-  loading.classList.add('loaded'); 
-}
-
-// 指定秒後に動作させる
-window.setTimeout( animation, 2000 );
-
-
 const scrollContainer = document.getElementById('scroll-container');
 
-// 横スクロールに変換するためのイベントリスナー
+// ホイールイベントをリスン
 scrollContainer.addEventListener('wheel', (event) => {
-  event.preventDefault();
-  scrollContainer.scrollLeft += event.deltaY * 2; // スクロール速度を調整（倍率を調整可能）
-});
+  event.preventDefault();  // デフォルトの縦スクロール動作を防止
 
+  // deltaYを使って横スクロールに変換
+  scrollContainer.scrollLeft += event.deltaY * 2;  // スクロール速度を調整
+});
